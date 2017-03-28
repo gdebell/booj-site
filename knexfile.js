b6e4701d617d2b2c240c051691796d1ac183e853
Gina <gdebell@gmail.com>
2017-03-28 12:34:18 -0600

    added production env to knex.js file

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const databaseName = 'Listings';
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: `postgres://localhost:5432/${databaseName}`,
+    connection: `postgres://localhost:5432/${Listings}`,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
@@ -13,12 +13,22 @@ module.exports = {
   },
   test: {
     client: 'postgresql',
-    connection: `postgres://localhost:5432/${databaseName}_test`,
+    connection: `postgres://localhost:5432/${Listings}_test`,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${Listings}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
 };
