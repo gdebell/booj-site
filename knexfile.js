8dab57a8d5245e7d9293805e0655d5770529e1c2
Gina <gdebell@gmail.com>
2017-03-28 13:33:25 -0600

    init

--- knexfile.js
+++ knexfile.js
@@ -11,17 +11,8 @@ module.exports = {
       directory: __dirname + '/src/server/db/seeds'
     }
   },
-  test: {
-    client: 'postgresql',
-    connection: `postgres://localhost:5432/${databaseName}_test`,
-    migrations: {
-      directory: __dirname + '/src/server/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/src/server/db/seeds'
-    }
-  },
-  pro: {
+
+  production: {
     client: 'postgresql',
     connection: `postgres://localhost:5432/${databaseName}`,
     migrations: {
