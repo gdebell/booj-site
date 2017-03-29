6a095b7073b06ae82d25894ca5b386f7d7e53498
Gina <gdebell@gmail.com>
2017-03-29 08:16:36 -0600

    layout

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -7,7 +7,5 @@
 const environment = process.env.NODE_ENV;
 const config = require('../../../knexfile.js')[environment];
 const knex = require('knex')(config);
-// for heroku
-//knex.migrate.latest();
-//knex.seed.run();
+
 module.exports = knex;
